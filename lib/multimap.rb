413cf7dab4ea47d755f3f4b310fe87154faaf48d
Joshua Peek <josh@joshpeek.com>
2009-05-26 17:48:30 -0500

    more multimap docs

--- lib/multimap.rb
+++ lib/multimap.rb
@@ -40,7 +40,7 @@ class Multimap < Hash
     super
   end
 
-  def initialize_copy(original)
+  def initialize_copy(original) #:nodoc:
     super
     clear
     original.each_pair { |key, container| self[key] = container }
@@ -52,6 +52,18 @@ class Multimap < Hash
   alias_method :hash_aset, :[]=
   private :hash_aset
 
+  # call-seq:
+  #   map[key] = value        => value
+  #   map.store(key, value)   => value
+  #
+  # Associates the value given by <i>value</i> with the key
+  # given by <i>key</i>. Unlike a regular hash, multiple can be
+  # assoicated with the same value.
+  #
+  #   map = Multimap["a" => 100, "b" => 200]
+  #   map["a"] = 9
+  #   map["c"] = 4
+  #   map   #=> {"a" => [100, 9], "b" => [200], "c" => [4]}
   def store(key, value)
     update_container(key) do |container|
       container << value
@@ -79,12 +91,40 @@ class Multimap < Hash
     end
   end
 
+  # call-seq:
+  #   map.each { |key, value| block } => map
+  #
+  # Calls <i>block</i> for each key/value pair in <i>map</i>, passing
+  # the key and value to the block as a two-element array.
+  #
+  #   map = Multimap["a" => 100, "b" => [200, 300]]
+  #   map.each {|key, value| puts "#{key} is #{value}" }
+  #
+  # <em>produces:</em>
+  #
+  #   a is 100
+  #   b is 200
+  #   b is 300
   def each
     each_pair do |key, value|
       yield [key, value]
     end
   end
 
+  # call-seq:
+  #   map.each_key { |key| block } => map
+  #
+  # Calls <i>block</i> for each key in <i>hsh</i>, passing the key
+  # as a parameter.
+  #
+  #   map = Multimap["a" => 100, "b" => [200, 300]]
+  #   map.each_key { |key| puts key }
+  #
+  # <em>produces:</em>
+  #
+  #   a
+  #   b
+  #   b
   def each_key
     each_pair_list do |key, values|
       yield key
@@ -96,6 +136,20 @@ class Multimap < Hash
 
   alias_method :each_pair_list, :each_pair
 
+  # call-seq:
+  #   map.each_pair { |key_value_array| block } => map
+  #
+  # Calls <i>block</i> for each key/value pair in <i>map</i>,
+  # passing the key and value as parameters.
+  #
+  #   map = Multimap["a" => 100, "b" => [200, 300]]
+  #   map.each_pair { |key, value| puts "#{key} is #{value}" }
+  #
+  # <em>produces:</em>
+  #
+  #   a is 100
+  #   b is 200
+  #   b is 300
   def each_pair
     each_pair_list do |key, values|
       values.each do |value|
@@ -104,13 +158,27 @@ class Multimap < Hash
     end
   end
 
+  # call-seq:
+  #   map.each_value { |value| block } => map
+  #
+  # Calls <i>block</i> for each key in <i>map</i>, passing the
+  # value as a parameter.
+  #
+  #   map = Multimap["a" => 100, "b" => [200, 300]]
+  #   map.each_value { |value| puts value }
+  #
+  # <em>produces:</em>
+  #
+  #   100
+  #   200
+  #   300
   def each_value
     each_pair do |key, value|
       yield value
     end
   end
 
-  def freeze
+  def freeze #:nodoc:
     each_pair_list { |_, container| container.freeze }
     default.freeze
     super
@@ -131,10 +199,29 @@ class Multimap < Hash
   end
   alias_method :value?, :has_value?
 
+  # call-seq:
+  #   map.index(value)    => key
+  #
+  # Returns the key for a given value. If not found, returns
+  # <tt>nil</tt>.
+  #
+  #   map = Multimap["a" => 100, "b" => [200, 300]]
+  #   map.index(100)   #=> "a"
+  #   map.index(200)   #=> "b"
+  #   map.index(999)   #=> nil
   def index(value)
     invert[value]
   end
 
+  # call-seq:
+  #   map.replace(other_map) => map
+  #
+  # Replaces the contents of <i>map</i> with the contents of
+  # <i>other_map</i>.
+  #
+  #   map = Multimap["a" => 100, "b" => 200]
+  #   map.replace({ "c" => 300, "d" => 400 })
+  #   #=> Multimap["c" => 300, "d" => 400]
   def replace(other)
     case other
     when Array, Hash
@@ -146,6 +233,14 @@ class Multimap < Hash
     end
   end
 
+  # call-seq:
+  #   map.invert => multimap
+  #
+  # Returns a new multimap created by using <i>map</i>'s values as keys,
+  # and the keys as values.
+  #
+  #   map = Multimap["n" => 100, "m" => 100, "d" => [200, 300]]
+  #   map.invert #=> Multimap[100 => ["n", "m"], 200 => "d", 300 => "d"]
   def invert
     h = Multimap.new(default.dup)
     each_pair { |key, value| h[value] = key }
@@ -184,10 +279,31 @@ class Multimap < Hash
   end
   alias_method :length, :size
 
+  # call-seq:
+  #   map.merge(other_map) => multimap
+  #
+  # Returns a new multimap containing the contents of <i>other_map</i> and
+  # the contents of <i>map</i>.
+  #
+  #   map1 = Multimap["a" => 100, "b" => 200]
+  #   map2 = Multimap["a" => 254, "c" => 300]
+  #   map2.merge(map2) #=> Multimap["a" => 100, "b" => [200, 254], "c" => 300]
+  #   map1             #=> Multimap["a" => 100, "b" => 200]
   def merge(other)
     dup.update(other)
   end
 
+  # call-seq:
+  #   map.merge!(other_map)    => multimap
+  #   map.update(other_map)    => multimap
+  #
+  # Adds each pair from <i>other_map</i> to <i>map</i>.
+  #
+  #   map1 = Multimap["a" => 100, "b" => 200]
+  #   map2 = Multimap["b" => 254, "c" => 300]
+  #
+  #   map1.merge!(map2)
+  #   #=> Multimap["a" => 100, "b" => [200, 254], "c" => 300]
   def update(other)
     case other
     when Hash
@@ -209,6 +325,15 @@ class Multimap < Hash
   end
   alias_method :merge!, :update
 
+  # call-seq:
+  #   map.select { |key, value| block }   => multimap
+  #
+  # Returns a new Multimap consisting of the pairs for which the
+  # block returns true.
+  #
+  #   map = Multimap["a" => 100, "b" => 200, "c" => 300]
+  #   map.select { |k,v| k > "a" }  #=> Multimap["b" => 200, "c" => 300]
+  #   map.select { |k,v| v < 200 }  #=> Multimap["a" => 100]
   def select
     inject(self.class.new) { |map, (key, value)|
       map[key] = value if yield([key, value])

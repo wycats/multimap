c1b781887f12286666770c0f8ca75fb3593032b2
Joshua Peek <josh@joshpeek.com>
2009-05-26 11:47:57 -0500

    add multimap construction benchmark

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,7 +6,6 @@ describe "Multimap project" do
 
   it "should have 100% test coverage"
 
-  it "should have a folder of sample benchmark"
   it "should be really fucking fast"
 
   it "should release a gem on GitHub"

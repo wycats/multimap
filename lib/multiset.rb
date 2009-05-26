a7a4ae55f68ddf87db7046138c44c326893b4176
Joshua Peek <josh@joshpeek.com>
2009-05-25 20:44:19 -0500

    doc defined multiset methods

--- lib/multiset.rb
+++ lib/multiset.rb
@@ -1,19 +1,24 @@
 require 'set'
 
+# Multiset implements a collection of unordered values and
+# allows duplicate values.
 class Multiset < Set
   attr_reader :hash
   protected :hash
 
+  # Returns the number of times an element belongs to the multiset.
   def multiplicity(e)
     @hash[e]
   end
 
+  # Returns the total number of elements in a multiset, including repeated memberships
   def cardinality
     @hash.inject(0) { |s, (e, m)| s += m }
   end
   alias_method :size, :cardinality
   alias_method :length, :cardinality
 
+  #--
   # def replace(enum)
   #   if enum.class == self.class
   #     @hash.replace(enum.hash)
@@ -24,35 +29,48 @@ class Multiset < Set
   # 
   #   self
   # end
+  #++
 
+  # Converts the set to an array. The order of elements is uncertain.
   def to_a
     inject([]) { |ary, (key, _)| ary << key }
   end
 
+  #--
   # def superset?(set)
   #   set.is_a?(Set) or raise ArgumentError, "value must be a set"
   #   return false if size < set.size
   #   set.all? { |o| include?(o) }
   # end
+  #++
 
+  #--
   # def proper_superset?(set)
   #   set.is_a?(Set) or raise ArgumentError, "value must be a set"
   #   return false if size <= set.size
   #   set.all? { |o| include?(o) }
   # end
+  #++
 
+  #--
   # def subset?(set)
   #   set.is_a?(Set) or raise ArgumentError, "value must be a set"
   #   return false if set.size < size
   #   all? { |o| set.include?(o) }
   # end
+  #++
 
+  #--
   # def proper_subset?(set)
   #   set.is_a?(Set) or raise ArgumentError, "value must be a set"
   #   return false if set.size <= size
   #   all? { |o| set.include?(o) }
   # end
+  #++
 
+  # Calls the given block once for each element in the set, passing
+  # the element as parameter. Returns an enumerator if no block is
+  # given.
   def each
     @hash.each_pair do |key, multiplicity|
       multiplicity.times do
@@ -62,6 +80,8 @@ class Multiset < Set
     self
   end
 
+  # Adds the given object to the set and returns self. Use +merge+ to
+  # add many elements at once.
   def add(o)
     @hash[o] ||= 0
     @hash[o] += 1
@@ -71,19 +91,26 @@ class Multiset < Set
 
   undef :add?
 
+  #--
+  # # Deletes the given object from the set and returns self. Use +subtract+ to
+  # # delete many items at once.
   # def delete(o)
   #   @hash.delete(o)
   #   self
   # end
+  #++
 
   undef :delete?
 
+  #--
   # def delete_if
   #   block_given? or return enum_for(__method__)
   #   to_a.each { |o| @hash.delete(o) if yield(o) }
   #   self
   # end
+  #++
 
+  #--
   # def merge(enum)
   #   if enum.instance_of?(self.class)
   #     @hash.update(enum.hash)
@@ -93,37 +120,52 @@ class Multiset < Set
   # 
   #   self
   # end
+  #++
 
+  #--
   # def subtract(enum)
   #   enum.each { |o| delete(o) }
   #   self
   # end
+  #++
 
+  #--
   # def |(enum)
   #   dup.merge(enum)
   # end
   # alias + |
   # alias union |
+  #++
 
+  #--
   # def -(enum)
   #   dup.subtract(enum)
   # end
   # alias difference
+  #++
 
+  #--
   # def &(enum)
   #   n = self.class.new
   #   enum.each { |o| n.add(o) if include?(o) }
   #   n
   # end
   # alias intersection &
+  #++
 
+  #--
   # def ^(enum)
   #   n = Set.new(enum)
   #   each { |o| if n.include?(o) then n.delete(o) else n.add(o) end }
   #   n
   # end
+  #++
 
-  def ==(set)
+  # Returns true if two sets are equal. Two multisets are equal if
+  # they have the same cardinalities and each element has the same
+  # multiplicity in both sets. The equality of each element inside
+  # the multiset is defined according to Object#eql?.
+  def eql?(set)
     return true if equal?(set)
 
     set = self.class.new(set) unless set.is_a?(self.class)
@@ -138,8 +180,5 @@ class Multiset < Set
 
     true
   end
-
-  def eql?(obj)
-    obj.is_a?(self.class) && self == obj
-  end
+  alias_method :==, :eql?
 end

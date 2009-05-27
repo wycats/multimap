23dd190c872b5b19cf3c6e741bf57dc1336c1068
Joshua Peek <josh@joshpeek.com>
2009-05-26 21:40:42 -0500

    remove project todos

--- spec/project_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Multimap project" do
-  it "should have 100% test coverage"
-
-  it "should be really fucking fast"
-
-  it "should release a gem on GitHub"
-  it "should bundle native code in gem"
-  it "should release a gem on RubyForge"
-end
